09f9d879229eee92e8490fee2d30cdde55838c26
utk1106 <thaokarutkarsh@gmail.com>
2021-12-13 22:06:13 +0530

    make your snake as long as you can :wink:
    make your snake as long as you can :wink:

---
+++ main.py
@@ -0,0 +1,60 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    # if head collides with any segment in the tail:
+    # trigger game_over
+# segment_1 = Turtle(shape="square")
+# segment_1.color("white")
+#
+# segment_2 = Turtle(shape="square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+#
+# segment_3 = Turtle(shape="square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+
+
+screen.exitonclick()
